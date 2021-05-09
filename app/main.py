dcebf9142b490847c24f988838efeae28f6106f6
k2pel <64159134+k2pel@users.noreply.github.com>
2021-05-09 06:41:17 +0200

    Create two input functions, organize inputs in main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from classes.gameplay.Gameplay import Gameplay
 from classes.real.RealPlayer import RealPlayer
-from functions.UserInteraction import input_alphabet_length
+from functions.UserInteraction import input_alphabet_length, input_word_length, input_game_time
 from colorama import Fore, Back, Style
+import random
 
 computer_players = {"1": "Losowy", "2": "Wykluczający", "3": "Przewidujący"}
 
@@ -16,15 +17,26 @@ if __name__ == "__main__":
     try:
         alphabet_length = input_alphabet_length()
     except:
-        print("Błędne dane. Ustawiam długość na 10.")
-        alphabet_length = 10
+        print("Poważny błąd przy wprowadzaniu danych. Losuję wielkość alfabetu.")
+        alphabet_length = random.randint(2,26)
     alphabet = [chr(i) for i in range(97, 97+alphabet_length)]
-
     print("Wybrany przez Ciebie alfabet to: " + str(alphabet))
-    word_length = int(input("Wybierz dlugosc slowa, ktore konczy gre: "))
-    # dolozyc obsluge wyjatku i wgl moze ladniej
 
-    game_time = int(input("Wybierz przez ile rund ma toczyc sie gra: "))
+    try:
+        word_length = input_word_length()
+    except:
+        print("Poważny błąd przy wprowadzaniu danych. Losuję długość słowa.")
+        word_length = random.randint(2,100)
+    print("Wybrano słowo o długości ", word_length, " liter.")
+
+
+    try:
+        game_time = input_game_time(word_length)
+    except:
+        print("Poważny błąd przy wprowadzaniu danych. Losuję długość słowa.")
+        game_time = random.randint(word_length, 100)
+    print("Wybrano ", game_time, " rund.")
+
     real_player = RealPlayer()
 
     gameplay = Gameplay(real_player, alphabet_length, alphabet,  word_length, game_time, type)
